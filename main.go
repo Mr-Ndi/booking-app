f309d5a70c2233cb23d3c2f7a9ae472ee1da5c1c
Mr-Ndi <onlythenotes@gmail.com>
2024-12-15 17:36:44 +0200

    User inputs and pointers

--- main.go
+++ main.go
@@ -8,18 +8,32 @@ func main() {
 	var remainingTicket = 50
 
 	//This is for printing the datatypes
-	fmt.Printf("ConferenceName is \t:%T, remainingTicket is\t: %T ,and conferenceTicket is \t:%T\n", ConferenceName, remainingTicket, conferenceTicket)
+	// fmt.Printf("ConferenceName is \t:%T, remainingTicket is\t: %T ,and conferenceTicket is \t:%T\n", ConferenceName, remainingTicket, conferenceTicket)
 
 	fmt.Printf("Wellcome to %v booking application !\n", ConferenceName)
 	fmt.Printf("We have a total of %v and %v are still available\n", conferenceTicket, remainingTicket)
 	fmt.Printf("Get your ticket here and attend  :)\n")
 
-	var userName string
+	fmt.Printf("\n--------------------------------------------------------------\n")
+
+	var firstName string
+	var lastName string
+	var email string
 	var userTicket int
 
-	// then lets assumes that the user will enter the name and nbr of tickets
-	userName = "Serge"
-	userTicket = 2
+	fmt.Printf("Then what's your first name pls :)\t:")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("%v Has booked %v tickets\n", userName, userTicket)
+	fmt.Printf("Then what's your first name pls :)\t:")
+	fmt.Scan(&lastName)
+
+	fmt.Printf("Then %v how many ticket you have booked\t:", firstName)
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("Then what's your email address :)\t:")
+	fmt.Scan(&email)
+	// userName = "Serge"
+	// userTicket = 2
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTicket, email)
 }
