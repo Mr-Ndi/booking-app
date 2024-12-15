7313993f1953c1e954b5dfdfd7bbcd454d3d8080
Mr-Ndi <onlythenotes@gmail.com>
2024-12-15 16:09:22 +0200

    Wellcomming message

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ConferenceName = "Go conference"
+
+	fmt.Println("Wellcome to", ConferenceName, " booking application !")
+	fmt.Println("Get your ticket here and attend  :)")
+
+}
